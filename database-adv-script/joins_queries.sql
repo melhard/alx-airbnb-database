72668386048efe19136c2d5d942fe0240754dedf
elhard <elhardmarouan15@gmail.com>
2025-05-15 13:09:22 +0100

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,28 @@
+-- INNER JOIN: bookings and users
+SELECT
+    bookings.id AS booking_id,
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+
+-- LEFT JOIN: properties and reviews
+SELECT
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+-- FULL OUTER JOIN: users and bookings
+SELECT
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
